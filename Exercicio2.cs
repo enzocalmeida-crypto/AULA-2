45fbbd6d0d09f9cf0aa70d2ca57fe4d8cde9d189
enzocalmeida-crypto <enzo.c.almeida@edu.senai.br>
2026-02-19 11:34:14 -0300

    Entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,11 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    private string nome = "Jose Mateus";
+    
+    public static void Executar()
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
